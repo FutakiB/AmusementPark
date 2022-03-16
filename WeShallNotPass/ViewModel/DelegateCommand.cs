e0bd761c9bf85ba6ac49b5d63979771155306e76
simon <simon.vajda23@gmail.com>
2022-03-16 20:31:42 +0100

    ViewModel base classes created

---
+++ WeShallNotPass/ViewModel/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WeShallNotPass.ViewModel
+{
+    class DelegateCommand : ICommand
+    {
+        private Func<object, bool> canExecute;
+        private Action<object> execute;
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null) return true;
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+                execute(parameter);
+        }
+    }
+}
---
+++ WeShallNotPass/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeShallNotPass.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
