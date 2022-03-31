be35be251c9686fb085807e32c3370b4fa48c1b0
FutakiB <lklt79@inf.elte.hu>
2022-03-31 13:56:20 +0200

    Message Event Args

---
+++ WeShallNotPass/Model/ErrorMessageEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeShallNotPass.Model
+{
+    internal class ErrorMessageEventArgs: EventArgs
+    {
+        private readonly string _message;
+
+        public ErrorMessageEventArgs(string message)
+        {
+            this._message = message;
+        }
+
+        public string Test
+        {
+            get { return this._message; }
+        }
+    }
+}
