e6c52bb9322cb36f833c70260dc92ad89c4a1bfe
Simon Vajda <simon.vajda23@gmail.com>
2022-04-07 14:07:02 +0200

    Test package added

---
+++ WeShallNotPass.Test/ModelTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeShallNotPass.Test
+{
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
